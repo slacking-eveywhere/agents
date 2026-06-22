1d1f8e1480e7b15d998fe9b0b463a456554161a7
Luc Deplanche <luc.deplanche@cs-soprasteria.com>
2026-06-22 16:42:32 +0200

    wip

--- install.sh
+++ install.sh
@@ -14,6 +14,10 @@ if [ -d "$SKILL_FOLDER" ]; then
     rm -rf "$SKILL_FOLDER"
 fi
 
+if [ -d "$ZED_AGENTS_DIRNAME"/guidelines ]; then
+    rm -rf "$ZED_AGENTS_DIRNAME"/guidelines
+fi
+
 curl -L -sS https://raw.githubusercontent.com/slacking-eveywhere/agents/"$REFS"/AGENTS.md -o "$ZED_AGENTS_DIRNAME"/AGENTS.md
 curl -L -sS https://raw.githubusercontent.com/slacking-eveywhere/agents/"$REFS"/bin/skills.tar.gz | tar -xz -C "$HOME_AGENTS_DIRNAME"
 curl -L -sS https://raw.githubusercontent.com/slacking-eveywhere/agents/"$REFS"/bin/guidelines.tar.gz | tar -xz -C "$ZED_AGENTS_DIRNAME"
